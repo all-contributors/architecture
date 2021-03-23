ed2a9c8ed94586d17f7664e6e40eb66afbede512
Berkmann18 <maxieberkmann@gmail.com>
2021-03-23 15:03:37 +0000

    build(script): use the DSL instead of the pulled JSON

--- export.sh
+++ export.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-${STRUCTURIZR}/structurizr.sh export -workspace structurizr-62115-workspace.json -format plantuml/c4plantuml -output diagrams
+# ${STRUCTURIZR}/structurizr.sh export -workspace structurizr-62115-workspace.json -format plantuml/c4plantuml -output diagrams
+${STRUCTURIZR}/structurizr.sh export -workspace ac.dsl -format plantuml/c4plantuml -output diagrams
